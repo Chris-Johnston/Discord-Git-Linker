191227a280c05e9b72bc0b77d3330e2a81c1f6aa
Chris Johnston <chris@thejohnstons.net>
2018-08-09 23:05:49 -0700

    standardize the naming patterns for the db tables

--- setup_database.py
+++ setup_database.py
@@ -21,46 +21,42 @@ if __name__ == '__main__':
     c = database.cursor()
 
     # discord user ID to github auth token table
-    c.execute("""CREATE TABLE IF NOT EXISTS userauth
-                            (id INT PRIMARY KEY,
-                            discordUserID TEXT,
-                            githubAuthorizationToken TEXT);""")
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS UserGithubAuthorization
+            (UserAuthorizationId INT PRIMARY KEY,
+             DiscordUserId TEXT,
+             GithubAuthorizationToken TEXT);''')
 
     # temporary login link table
     # this stores single use tokens that are sent as a DM to users
     # which are used to log a user into this service
     c.execute('''
-                CREATE TABLE IF NOT EXISTS
-                login
-                (
-                loginId INT PRIMARY KEY,
-                discordUserID UNSIGNED BIG INT,
-                token TEXT,
-                expiration DATETIME
-                )''')
+        CREATE TABLE IF NOT EXISTS UserLogin
+        (UserLoginId INT PRIMARY KEY,
+         DiscordUserId UNSIGNED BIG INT,
+         Token TEXT,
+         Expiration DATETIME);''')
 
     # channel link table
     # links a channel and guild id to a repo in the format user/repoName
     c.execute('''
-        CREATE TABLE IF NOT EXISTS link_channels
-        ( guildId UNSIGNED BIG INT,
-          channelId UNSIGNED BIG INT,
-          authorUserId UNSIGNED BIG INT,
-          createdAt DATETIME,
-          repoUrl TEXT
-          )''')
+        CREATE TABLE IF NOT EXISTS LinkChannels
+        (GuildId UNSIGNED BIG INT,
+         ChannelId UNSIGNED BIG INT,
+         AuthorDiscordUserId UNSIGNED BIG INT,
+         CreatedAt DATETIME,
+         RepoName TEXT);''')
 
     # links a guild to a repo in the format user/repoName
     # the authorOnly flag allows for this behavior to only work for the user who
     # made the link, effectively enabling a per-user link
     c.execute('''
-            CREATE TABLE IF NOT EXISTS link_guilds
-            ( guildId UNSIGNED BIG INT,
-              authorUserId UNSIGNED BIG INT,
-              createdAt DATETIME,
-              repoUrl TEXT,
-              authorOnly INT
-              )''')
+        CREATE TABLE IF NOT EXISTS LinkGuilds
+        (GuildId UNSIGNED BIG INT,
+         AuthorDiscordUserId UNSIGNED BIG INT,
+         CreatedAt DATETIME,
+         RepoName TEXT,
+         AuthorOnly INT);''')
 
     # close cursor when done
     c.close()
